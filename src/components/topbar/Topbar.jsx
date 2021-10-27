9c0b6d71b3f723f34d27b5e146f7f01a08e4b938
David Agaybi <davidagaybi@gmail.com>
2021-10-27 12:43:07 -0400

    second commit

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,8 +2,15 @@ import './topbar.scss';
 
 export default function topbar() {
     return (
-        <div className="topbar">
-            
+        <div className="topbar" id="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro">sfsdfsdf</a>
+                </div>
+                <div className="right">
+
+                </div>
+            </div>
         </div>
     )
 }
